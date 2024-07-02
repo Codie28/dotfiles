ee1a27ddc31b84cbfcefe60ad03145cde159fdd7
Codie <codieuwu+git@gmail.com>
2024-07-02 17:45:12 +0300

    fixed Ctrl + hjkl changing windows on tmux for some fucking reason

--- .config/nvim/lua/codie/remaps.lua
+++ .config/nvim/lua/codie/remaps.lua
@@ -11,3 +11,8 @@ vim.keymap.set('v', '<Left>', '')
 vim.keymap.set('v', '<Right>', '')
 vim.keymap.set('v', '<Up>', '')
 vim.keymap.set('v', '<Down>', '')
+
+vim.keymap.set('n', '<C-h>', '')
+vim.keymap.set('n', '<C-j>', '')
+vim.keymap.set('n', '<C-k>', '')
+vim.keymap.set('n', '<C-l>', '')
