1684a8297ed28857437583288066c0fd87462a07
Codie28 <codieuwu+git@gmail.com>
2024-08-04 17:43:28 +0300

    changed theme to catpuccin-fraple

--- .config/nvim/lua/codie/colors.lua
+++ .config/nvim/lua/codie/colors.lua
@@ -6,7 +6,7 @@ vim.g.moonlight_italic_keywords = false
 -- vim.g.moonlight_borders = false 
 -- vim.g.moonlight_disable_background = false
 
-vim.cmd.colorscheme('moonlight')
+vim.cmd.colorscheme('catppuccin-frappe')
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
--- .config/nvim/lua/codie/lazy.lua
+++ .config/nvim/lua/codie/lazy.lua
@@ -13,6 +13,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plug = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
