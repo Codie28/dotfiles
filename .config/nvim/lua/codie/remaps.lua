349a7767ee614357b105ae72e96c00489dc808ae
Codie28 <codieuwu+git@gmail.com>
2024-08-10 14:43:39 +0300

    some more remaps

--- .config/nvim/lua/codie/remaps.lua
+++ .config/nvim/lua/codie/remaps.lua
@@ -14,7 +14,15 @@ vim.keymap.set('v', '<Down>', '')
 
 vim.keymap.set('n', 'ı', 'i')
 
--- vim.keymap.set('n', '<C-h>', '')
--- vim.keymap.set('n', '<C-j>', '')
--- vim.keymap.set('n', '<C-k>', '')
--- vim.keymap.set('n', '<C-l>', '')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Seperate vim and system yank
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
