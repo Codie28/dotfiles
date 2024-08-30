b17a9b97d02e8f83f4083fe5fd13a11594d20000
Codie28 <codieuwu+git@gmail.com>
2024-08-30 10:26:52 +0300

    new switch and polybar corrections

---
+++ mybin/cht.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+langs="js lua"
+utils="jq awk sed sort find"
+
+selec=`printf "$langs $utils" | tr ' ' '\n' | sort -r | fzf --preview "curl cht.sh/{} -s"`
+
+if echo $langs | grep "$selec"; then
+  printf 'Lang query: '
+  read query
+  curl cht.sh/$selec/`echo $query | tr ' ' '+'` -s | less -r
+else
+  curl cht.sh/$selec | less -r
+fi
