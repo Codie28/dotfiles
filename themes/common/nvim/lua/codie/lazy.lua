729a09324a19ffbb8915bb02a058d599ab57eb3f
Codie28 <codieuwu+git@gmail.com>
2024-09-06 19:12:03 +0300

    formated lazy.lua

--- themes/common/nvim/lua/codie/lazy.lua
+++ themes/common/nvim/lua/codie/lazy.lua
@@ -1,11 +1,11 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -13,52 +13,30 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plug = {
-  -- maybe a way to add theme spesific plugins?
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-  },
-  {
-    "mbbill/undotree"
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    }
-  },
-  {
-  	"folke/which-key.nvim",
-    lazy = false,
- 	  init = function()
- 	    vim.o.timeout = true
- 	    vim.o.timeoutlen = 500
- 	  end,
- 	  opts = {}
-  },
-  { "alexghergh/nvim-tmux-navigation" },
+  --- maybe a way to add theme specific plugins?
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.5' },
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  { 'nvim-lua/plenary.nvim' },
+  { 'nvim-tree/nvim-tree.lua' },
+  { 'nvim-tree/nvim-web-devicons' },
+  { 'folke/which-key.nvim' },
+  { 'mbbill/undotree' },
 
-  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-  {'williamboman/mason.nvim'},
-  {'williamboman/mason-lspconfig.nvim'},
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
 
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-  {'neovim/nvim-lspconfig'},
-  {'hrsh7th/cmp-nvim-lsp'},
-  {'hrsh7th/nvim-cmp'},
-  -- {'L3MON4D3/LuaSnip'},
+  { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+  { 'neovim/nvim-lspconfig' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'L3MON4D3/LuaSnip' },
+
   {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' }
   },
 }
 
-require("lazy").setup(plug)
+require('lazy').setup(plug)
