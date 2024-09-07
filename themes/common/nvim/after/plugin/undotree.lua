8d9135dbce5a1d8532d807e89e113952b83a6456
Codie28 <codieuwu+git@gmail.com>
2024-09-07 18:35:45 +0300

    streamlined undotree keymaps to be like nvimtree

--- themes/common/nvim/after/plugin/undotree.lua
+++ themes/common/nvim/after/plugin/undotree.lua
@@ -1,4 +1,8 @@
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {desc='Open undotree'})
+vim.keymap.set('n', '<leader>u', function()
+  vim.cmd.UndotreeShow()
+  vim.cmd.UndotreeFocus()
+end, {desc='Focus undotree'})
+vim.keymap.set('n', '<C-h>', vim.cmd.UndotreeToggle, {desc='Toggle undotree'})
 
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
