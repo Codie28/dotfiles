f69b5bf4aabc121ac780b4e1d7948cc752546b12
Codie28 <codieuwu+git@gmail.com>
2024-09-08 17:19:04 +0300

    resource colors.lua on SIGUSR1 singnal

--- themes/common/nvim/lua/codie/signals.lua
+++ themes/common/nvim/lua/codie/signals.lua
@@ -1,6 +1,7 @@
 local signal = vim.uv.new_signal()
 vim.uv.signal_start(signal, "sigusr1", function(signal)
   vim.schedule(function()
+    vim.cmd("so $HOME/.config/nvim/lua/codie/colors.lua")
     colormycursors()
   end)
 end)
